54b4aecccdd1dc444a87b696a530f4bd217a0f04
Conner <bilec@ualberta.ca>
2014-02-08 14:50:58 -0700

    First part of the user interface

--- CMPUT301W14T09/src/ca/cmput301w14t09/TopCommentsActivity.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/TopCommentsActivity.java
@@ -1,21 +1,23 @@
-package ca.example.cmput301w14t09;
+package ca.cmput301w14t09;
+
+
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class TopCommentsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_top_comments);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.top_comments, menu);
 		return true;
 	}
 

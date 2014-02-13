d7f16c02a9891cf80cf1777c7139f580c0d95d29
cntnboys <cpalexan@ualberta.ca>
2014-02-13 16:29:26 -0700

    added in some storyboard pictures

--- CMPUT301W14T09/src/view/MainActivity.java
+++ CMPUT301W14T09/src/view/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(ca.geocomment.R.layout.activity_main);
 	}
 
 	@Override

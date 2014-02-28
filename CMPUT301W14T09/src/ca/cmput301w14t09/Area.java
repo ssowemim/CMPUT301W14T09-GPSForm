99bd9ce667e2e85d687a517667a8fd5f2b9ea3c3
cntnboys <cpalexan@ualberta.ca>
2014-02-27 18:42:55 -0700

    added build areas and organized a bit

--- CMPUT301W14T09/src/ca/cmput301w14t09/Area.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/Area.java
@@ -4,29 +4,24 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 
-
-
-
 public class Area {
-
 	
+	private String areaName;
+
 	private ArrayList<GeoLocation> containedGeoLocations;
 
-	
 	public ArrayList<GeoLocation> getContainedGeoLocations() {
 		return containedGeoLocations;
 	}
-
 	
+	private Collection<GeoLocation> geoLocation;
+
+
 	public void setContainedGeoLocations(
 			ArrayList<GeoLocation> containedGeoLocations) {
 				this.containedGeoLocations = containedGeoLocations;
 			}
 
-	
-	private String areaName;
-
-	//test sfd
 	public String getAreaName() {
 		return areaName;
 	}
@@ -36,10 +31,6 @@ public class Area {
 		this.areaName = areaName;
 	}
 
-	
-	private Collection<GeoLocation> geoLocation;
-
-	
 	public Collection<GeoLocation> getGeoLocation() {
 		return geoLocation;
 	}
@@ -49,7 +40,6 @@ public class Area {
 		this.geoLocation = geoLocation;
 	}
 
-		
 		public boolean containsGeoLocation(GeoLocation location){
 			return false;	
 		}
---
+++ CMPUT301W14T09/src/ca/cmput301w14t09/BuildAreas.java
@@ -0,0 +1,11 @@
+package ca.cmput301w14t09;
+
+
+//class that is responsible for building the areas needed for geolocations.
+
+public class BuildAreas {
+	
+	
+	
+
+}

9e6e47609e3a9903001bb3681945e7197bda2981
cntnboys <cpalexan@ualberta.ca>
2014-02-27 20:21:35 -0700

    logging some geolocations on campus

--- CMPUT301W14T09/src/ca/cmput301w14t09/BuildAreas.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/BuildAreas.java
@@ -1,11 +1,27 @@
 package ca.cmput301w14t09;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.content.Context;
+
 
 //class that is responsible for building the areas needed for geolocations.
 
+
 public class BuildAreas {
 	
+	List<Area> listAreas = new ArrayList<Area>();
 	
+public List<Area> AreaBuild(Context appcontext){
+	
+	
+	
+	
+	return listAreas;
+	
+		}
 	
 
 }

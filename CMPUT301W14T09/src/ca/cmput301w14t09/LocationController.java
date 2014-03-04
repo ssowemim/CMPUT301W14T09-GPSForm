0513fe790dcd788d0652ed4bec795b4ce9766db5
cntnboys <cpalexan@ualberta.ca>
2014-03-03 20:38:56 -0700

    refactoring geolocation files

--- CMPUT301W14T09/src/ca/cmput301w14t09/LocationController.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/LocationController.java
@@ -46,86 +46,50 @@ public class LocationController {
 				}
 
 		
-		public LocationListener NewListener(final TextView tv, final EditText tv2, final EditText tv3){
+		public void locationchanged(android.location.Location location, EditText tv2, EditText tv3){
 			
-			// Retrieve location updates through LocationListener interface
-			final LocationListener locationListener = new LocationListener(){
-			// To Do: override the four methods.
-				
-				
-				public void onProviderDisabled (String provider){
-					
-				}
-				
-				public void onProviderEnabled (String provider){
-					
-					
-				}
-				
-				public void onStatusChanged (String provider, int status, Bundle extras){
-					
-					
-				}
-
-				@Override
-				public void onLocationChanged(android.location.Location location) {
-					// TODO Auto-generated method stub
-					
-					if(location != null){
-						
-						lat = location.getLatitude();
-						lng = location.getLongitude();
-						
-						
-						Date date = new Date(location.getTime());
-						tv.setText("your location is " +lat+"\n" + lng +"\n" + date.toString());
-						tv2.setText(""+lng);
-						tv3.setText(""+lat);
-						System.out.println("got here");
-						
-					}else{
-						tv.setText("no location information");
-					}
-				}
-			};
-			return locationListener;
-		}
-
-		
-		public OnClickListener UpdateListener(Button btnSimple2, final EditText tv2, final EditText tv3){
-			//Button increments count, Click me
+			// TODO Auto-generated method stub
 			
-			
-			btnSimple2.setOnClickListener(new View.OnClickListener() {
+			if(location != null){
 				
-			@Override
-			public void onClick(View v) {
-					
-				//System.out.println("UPdate complete");
+				lat = location.getLatitude();
+				lng = location.getLongitude();
 				
-				//lng
-				String text = tv2.getText().toString();
 				
-				//lat
-				String text1 = tv3.getText().toString();
-				
-				//lng
-				double double1 = Double.parseDouble(text);
-				lng = double1;
-				
-				//lat
-				double double2 = Double.parseDouble(text1);
-				lat = double2;
-				
-				//sets geolocation to new given one
+				Date date = new Date(location.getTime());
+				//tv.setText("your location is " +lat+"\n" + lng +"\n" + date.toString());
+				tv2.setText(""+lng);
+				tv3.setText(""+lat);
+				System.out.println("got here");
+				//set geolocation to current location
 				setGeoLocation();
 				
-				
-				 
+			}else{
+				//tv.setText("no location information");
 			}
-	 });
-			return null;
-				
+			
+		}
+			
+		
+		public void updatelocation(EditText tv2, EditText tv3){
+			//System.out.println("UPdate complete");
+			
+			//lng
+			String text = tv2.getText().toString();
+			
+			//lat
+			String text1 = tv3.getText().toString();
+			
+			//lng
+			double double1 = Double.parseDouble(text);
+			lng = double1;
+			
+			//lat
+			double double2 = Double.parseDouble(text1);
+			lat = double2;
+			
+			//sets geolocation to new given one
+			setGeoLocation();
 		}
 		
 }
--- CMPUT301W14T09/src/ca/cmput301w14t09/SelectLocationActivity.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/SelectLocationActivity.java
@@ -5,10 +5,9 @@ import android.content.Context;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View.OnClickListener;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class SelectLocationActivity extends Activity {
 	
@@ -17,34 +16,70 @@ public class SelectLocationActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_location);
 		
-		
 		//Get textviews
-		TextView tv = (TextView) findViewById(R.id.locationtext);
-		EditText tv2 = (EditText) findViewById(R.id.longtext3);
-		EditText tv3 = (EditText) findViewById(R.id.lattext3);
+		//TextView tv = (TextView) findViewById(R.id.locationtext);
+		final EditText tv2 = (EditText) findViewById(R.id.longtext3);
+		final EditText tv3 = (EditText) findViewById(R.id.lattext3);
 		
 		// Obtain LocationManager service 
 		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		
+		
 		//new Location Controller 
-		LocationController lc = new LocationController();
+		final LocationController lc = new LocationController();
+				
 		
-		//new Listener for location detecting
-		LocationListener locationListener = lc.NewListener(tv,tv2,tv3);
-	
-		//set up location update request.
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-		
-		//get button id
-		Button btnSimple2 = (Button) findViewById(R.id.changebutton);
-		
-		//new Listener for button update
-		OnClickListener onclickListener = lc.UpdateListener(btnSimple2, tv2, tv3);
-		
-		
-		//set geolocation to current location
-		lc.setGeoLocation();
-		
+		// Retrieve location updates through LocationListener interface
+		final LocationListener locationListener = new LocationListener(){
+		// To Do: override the four methods.
+						
+						
+						public void onProviderDisabled (String provider){
+							
+						}
+						
+						public void onProviderEnabled (String provider){
+							
+							
+						}
+						
+						public void onStatusChanged (String provider, int status, Bundle extras){
+							
+							
+						}
+
+						@Override
+						public void onLocationChanged(android.location.Location location) {
+							
+							lc.locationchanged(location, tv2, tv3);
+							
+							
+						}
+					};
+					
+					
+					
+					
+					
+					
+					//set up location update request.
+					lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+							
+				
+					
+					//update location button
+					Button btnSimple2 = (Button) findViewById(R.id.changebutton);
+					btnSimple2.setOnClickListener(new View.OnClickListener() {
+						
+						@Override
+						public void onClick(View v) {
+								
+							
+							lc.updatelocation(tv2, tv3);
+							
+							 
+						}
+				 });
 		
 		
 	}

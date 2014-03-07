7c38a2c4c27c9baebdec6fbbd6faaa3181ea4b0b
chunhanlee <chunhan@ualberta.ca>
2014-03-07 12:53:35 -0700

    Update Picture.java
    Reformated

--- CMPUT301W14T09/src/ca/cmput301w14t09/model/Picture.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/model/Picture.java
@@ -6,43 +6,19 @@ import java.util.Collection;
 
 public class Picture {
 
-	/**
-	 * @uml.property  name="MAX_SIZE" readOnly="true"
-	 */
 	private static int MAX_SIZE = 100;
+	private Collection<Comment> comment;
 
-		
-		/**
-		 */
-		public boolean validatePictureSize(android.graphics.Bitmap picture){
-			return false;	
-		}
+	public boolean validatePictureSize(android.graphics.Bitmap picture){
+		return false;	
+	}
 
+	public Collection<Comment> getComment() {
+		return comment;
+	}
 
-		/**
-		 * @uml.property   name="comment"
-		 * @uml.associationEnd   multiplicity="(0 -1)" inverse="picture:model.Comment"
-		 */
-		private Collection<Comment> comment;
-
-
-		/** 
-		 * Getter of the property <tt>comment</tt>
-		 * @return  Returns the comment.
-		 * @uml.property  name="comment"
-		 */
-		public Collection<Comment> getComment() {
-			return comment;
-		}
-
-
-		/** 
-		 * Setter of the property <tt>comment</tt>
-		 * @param comment  The comment to set.
-		 * @uml.property  name="comment"
-		 */
-		public void setComment(Collection<Comment> comment) {
-			this.comment = comment;
-		}
+	public void setComment(Collection<Comment> comment) {
+		this.comment = comment;
+	}
 
 }

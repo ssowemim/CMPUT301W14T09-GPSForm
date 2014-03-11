92c4d16d33112ec2a7256b7dfbaa66f96c371011
chunhanlee <chunhan@ualberta.ca>
2014-03-11 13:35:11 -0600

    deleted everything in master to clear conflict

--- CMPUT301W14T09/gen/ca/cmput301w14t09/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ca.cmput301w14t09;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- CMPUT301W14T09/src/ca/cmput301w14t09/view/SelectLocationActivity.java
+++
@@ -1,6 +0,0 @@
-package ca.cmput301w14t09.view;
-
-
-public class SelectLocationActivity {
-
-}

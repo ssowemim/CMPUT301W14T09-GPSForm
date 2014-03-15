2cb58d40febb4ac1edbda8610278d00163445425
chunhanlee <chunhan@ualberta.ca>
2014-03-15 14:42:08 -0600

    Added test files for Comment, CommentThread, Picture, Thread

--- CMPUT301W14T09Test/src/CommentTest.java
+++ CMPUT301W14T09Test/src/CommentTest.java
@@ -2,17 +2,37 @@ import static org.junit.Assert.*;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import ca.cmput301w14t09.Controller.LocationController;
 import ca.cmput301w14t09.FileManaging.CreateComment;
 
 
 public class CommentTest extends TestCase{
 
 	@Test
-	public void testaddComment() {
+	public void testAddComment() {
 		
 		
-		assertNotNull(CreateComment.newComment("tester", "testingcommentcache", true));
+		LocationController lc = new LocationController();
+		lc.setGeoLocation();
+		assertNotNull(CreateComment.newComment(lc, "tester", "testingcommentcache", true));
 		
 	}
+	
+	@Test
+	public void testAddNullComment() {
+		LocationController lc = new LocationController();
+		LocationController lcOne = new LocationController();
+		
+		assertFalse(CreateComment.newComment(lc, "testerUser", null, true).equals(CreateComment.newComment(lcOne,  "testerUser", "Bobby", true)));
+	}
+	
+	public void testIfTopComment() {
+		LocationController lc = new LocationController();
+		LocationController lcOne = new LocationController();
+		
+		assertFalse(CreateComment.newComment(lc, "testerUser", "testerComment", false).equals(CreateComment.newComment(lcOne, "testerUser", "testerComment", true)));
+	}
+	
+	
 
 }

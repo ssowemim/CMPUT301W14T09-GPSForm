1e9745be9767f210cc9d3bff5d6face3ddd82591
cntnboys <cpalexan@ualbeta.ca>
2014-04-02 12:18:21 -0600

    fixed some issues with the mapquest, made it a bit more user freindly

--- CMPUT301W14T09/src/ca/cmput301w14t09/Controller/SortingController.java
+++ CMPUT301W14T09/src/ca/cmput301w14t09/Controller/SortingController.java
@@ -26,8 +26,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-
 
 import ca.cmput301w14t09.Model.Comment;
 import ca.cmput301w14t09.Model.GeoLocation;

08b602d8f5861eb30e1794662960cf95e894301e
cntnboys <cpalexan@ualberta.ca>
2014-04-02 22:03:51 -0600

    did caching tests

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/CacheTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/CacheTest.java
@@ -11,6 +11,12 @@ import ca.cmput301w14t09.Model.Comment;
 
 public class CacheTest extends TestCase {
 	
+	private static final int ArrayList = 0;
+	private static final int Comment = 0;
+
+	/**
+	 * Cameron Alexander
+	 */
 	public void testcache(){
 			final int MAX_LENGTH = 200;
 	        ArrayList<Comment> comments = new ArrayList<Comment>(MAX_LENGTH);
@@ -18,7 +24,9 @@ public class CacheTest extends TestCase {
 	    }
 	
 	
-	
+	/**
+	 * Cameron Alexander
+	 */
 	public void testadd(){
 		Cache cache = new Cache();
 	    LocationController lc1 = new LocationController();
@@ -31,13 +39,49 @@ public class CacheTest extends TestCase {
 	    
 	    assertTrue(cache.getCache().size() == 1);
 	    
-	    
-	    
-	    
 		
 	}
 	
+	/**
+	 * Cameron Alexander
+	 */
+	public void testadd2(){
+		Cache cache = new Cache();
+		final int MAX_LENGTH = 200;
+		ArrayList<Comment> newComments = null;
+		LocationController lc1 = new LocationController();
+	    String text2 = "cameron";
+	    String text1 = "this is a comment";
+	    
+	    cache.add(newComments);
+		
+	}
 	
+	/**
+	 * Cameron Alexander
+	 */
+	public void testGetTopComments(){
+		Cache cache = new Cache();
+		boolean topcomments;
+		
+		ArrayList<Comment> comments = cache.getTopComments(true);
+		ArrayList<Comment> comments2 = cache.getTopComments(false);
+		
+		assertTrue(comments2 != comments);
+		
+	}
 
+	/**
+	 * Cameron Alexander
+	 */
+	public void testGetSubComments(){
+		Cache cache = new Cache();
 
+		ArrayList<Comment> subcomments = cache.getSubComments("12345");
+		ArrayList<Comment> subcomments2 = cache.getSubComments("1234");
+
+		assertTrue(subcomments2 != subcomments);
+	}
+	
+	
 }

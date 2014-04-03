d81242d1bd6fc7cd4a0ec02c97f7874392c3bb8a
ssowemim <ssowemim@ualberta.ca>
2014-04-02 21:33:23 -0600

    icon for change location

--- CMPUT301W14T09/gen/ca/cmput301w14t09/R.java
+++ CMPUT301W14T09/gen/ca/cmput301w14t09/R.java
@@ -23,9 +23,10 @@ public final class R {
     public static final class drawable {
         public static final int attachment_pic=0x7f020000;
         public static final int camera=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_location=0x7f020003;
-        public static final int no_img=0x7f020004;
+        public static final int ic_change_location=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_location=0x7f020004;
+        public static final int no_img=0x7f020005;
     }
     public static final class id {
         public static final int UserList=0x7f080004;

1759c4e2cd2043f5c161157c22329f19fecde536
chunhanlee <chunhan@ualberta.ca>
2014-04-03 23:12:16 -0600

    made test for SortingController not working yet

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/SortingControllerTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/SortingControllerTest.java
@@ -1,38 +1,42 @@
 package ca.cmput301w14t09.test;
 
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
 import ca.cmput301w14t09.TopCommentsActivity;
 import ca.cmput301w14t09.Controller.LocationController;
+import ca.cmput301w14t09.Controller.SortingController;
 import ca.cmput301w14t09.FileManaging.CommentFactory;
 import ca.cmput301w14t09.Model.Comment;
+import ca.cmput301w14t09.Model.GeoLocation;
 import ca.cmput301w14t09.elasticSearch.ElasticSearchOperations;
 import android.test.ActivityInstrumentationTestCase2;
 
+/**
+ * 
+ * @author chunhan
+ * Testing Sorting for comments
+ */
+public class SortingControllerTest extends TestCase {
 
-public class SortingControllerTest extends ActivityInstrumentationTestCase2<TopCommentsActivity> {
-
-	public SortingControllerTest(Class<TopCommentsActivity> name) {
-		super(name);
-	}
-
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	public void testSortingByDefaultLocation(){
+	public void testSortingCommentsByLocation(){
 		Comment comment = null;
 		LocationController lc = new LocationController();
+		GeoLocation geo = new GeoLocation();
+		ArrayList<Comment> commList = new ArrayList<Comment>();
+		SortingController sc = new SortingController();
+		geo.setLongitude(34.0);
+		lc.setGeo(geo);
+		
 		for (int i = 0; i< 4; i++){
+			geo.setLatitude(i);
+			lc.setGeo(geo);
 			comment = CommentFactory.buildComment(lc, "test"+i, "testing"+i, true, null);
-			try {
-				ElasticSearchOperations.postThread(comment);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			commList.add(comment);
 		}
-		try {
-			assertNotNull(ElasticSearchOperations.pullThreads());
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		geo.setLatitude(54.0);
+		ArrayList<Comment> testList = sc.sortTopComments(null, geo, commList);
+		String name = testList.get(0).getAuthorName();
+		assertTrue(name.equals("test3"));
 	}
 }

fe599c49c1bed5638791e46258a81b40826ef937
ssowemim <ssowemim@ualberta.ca>
2014-04-06 12:29:20 -0600

    Testing some more

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/UserProfileModelTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/UserProfileModelTest.java
@@ -9,12 +9,11 @@ import junit.framework.TestCase;
  * @author ssowemim
  *
  */
-public class UserProfileControllerTest extends ActivityInstrumentationTestCase2<TopCommentsActivity> {
+public class UserProfileModelTest extends ActivityInstrumentationTestCase2<TopCommentsActivity> {
 
-	public UserProfileControllerTest(Class<TopCommentsActivity> activityClass) {
+	public UserProfileModelTest(Class<TopCommentsActivity> activityClass) {
 		super(activityClass);
 		// TODO Auto-generated constructor stub
 	}
-
 
 }

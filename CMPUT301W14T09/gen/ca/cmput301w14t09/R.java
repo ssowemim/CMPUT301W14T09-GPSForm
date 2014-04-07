0a74cac1d407aa284e0e4c967e526452b665529a
chunhanlee <chunhan@ualberta.ca>
2014-04-07 13:57:07 -0600

    changed app name and the icon and on new elastic search

--- CMPUT301W14T09/gen/ca/cmput301w14t09/R.java
+++ CMPUT301W14T09/gen/ca/cmput301w14t09/R.java
@@ -142,6 +142,7 @@ public final class R {
         public static final int attachment_description=0x7f050008;
         public static final int camera_button_description=0x7f050006;
         public static final int favButton=0x7f05000a;
+        public static final int final_app_name=0x7f05000c;
         public static final int hello_world=0x7f050002;
         public static final int image_view_description=0x7f050007;
         public static final int title_activity_comment_list=0x7f050005;

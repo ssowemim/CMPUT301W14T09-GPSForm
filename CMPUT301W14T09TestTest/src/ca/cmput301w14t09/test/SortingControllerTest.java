9d91231c72f4ae2ecf7acdb9e9a141de2a63dd94
ssowemim <ssowemim@ualberta.ca>
2014-04-07 12:23:19 -0600

    Fixed sorting controller test

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/SortingControllerTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/SortingControllerTest.java
@@ -3,13 +3,17 @@ package ca.cmput301w14t09.test;
 import java.util.ArrayList;
 
 import junit.framework.TestCase;
+import ca.cmput301w14t09.R;
 import ca.cmput301w14t09.TopCommentsActivity;
 import ca.cmput301w14t09.Controller.LocationController;
 import ca.cmput301w14t09.Controller.SortingController;
 import ca.cmput301w14t09.FileManaging.CommentFactory;
+import ca.cmput301w14t09.FileManaging.SerializableBitmap;
 import ca.cmput301w14t09.Model.Comment;
 import ca.cmput301w14t09.Model.GeoLocation;
 import ca.cmput301w14t09.elasticSearch.ElasticSearchOperations;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.test.ActivityInstrumentationTestCase2;
 
 /**
@@ -17,38 +21,56 @@ import android.test.ActivityInstrumentationTestCase2;
  * @author Chun-Han Lee
  * Testing Sorting for comments
  */
-public class SortingControllerTest extends TestCase {
+public class SortingControllerTest extends ActivityInstrumentationTestCase2<TopCommentsActivity> {
 
+	public SortingControllerTest(){
+		super(TopCommentsActivity.class);
+	}
+	
 	/**
 	 * @author Chun-Han Lee
 	 */
 	public void testSortingCommentsByLocation(){
-		Comment comment = null;
+		Comment comment = null, comment1 = null;
 		LocationController lc = new LocationController();
+		LocationController lc1 = new LocationController();
+		LocationController lc2 = new LocationController();
+		
 		GeoLocation geo = new GeoLocation();
 		ArrayList<Comment> commList = new ArrayList<Comment>();
 		SortingController sc = new SortingController();
+		Bitmap bitmap =  BitmapFactory.decodeResource(this.getActivity().getResources(), R.drawable.no_img);
+		SerializableBitmap sBitmap = new SerializableBitmap(bitmap);
 		
-		geo.setLongitude(34.0);
-		geo.setLatitude(-111.0);
-		lc.setGeo(geo);
-		comment = CommentFactory.buildComment(lc, "test1", "testing1", true, null, null, null);
-		commList.add(comment);
+		geo.setLatitude(20.0);
+		geo.setLongitude(10.0);
+		lc1.setGeo(geo);
 		
-		geo.setLongitude(35.0);
-		geo.setLatitude(-1.0);
-		lc.setGeo(geo);
-		comment = CommentFactory.buildComment(lc, "test2", "testing2", true, null, null, null);
+		comment = CommentFactory.buildComment(lc1, "test1", "testing1", true, sBitmap, true, "username");
 		commList.add(comment);
 
-		geo.setLatitude(54.0);
-		geo.setLongitude(23.0);
+		geo.setLatitude(25.0);
+		geo.setLongitude(12.0);
+		lc2.setGeo(geo);
+		
+		comment1 = CommentFactory.buildComment(lc2, "test2", "testing2", true, sBitmap, true, "username1");
+		commList.add(comment1);
+
+		geo.setLongitude(0);
+		geo.setLatitude(0);
 		lc.setGeo(geo);
+		
 		ArrayList<Comment> testList = sc.sortTopComments(lc, null, commList);
 		String name = testList.get(0).getAuthorName();
 		String name2 = testList.get(1).getAuthorName();
-		assertTrue(name.contains("test1"));
-		assertTrue(name2.contains("test2"));
+		
+		// names are put into the list as follows
+		assertTrue(commList.get(0).getAuthorName().equals("test1"));
+		assertTrue(commList.get(1).getAuthorName().equals("test2"));
+
+		// after sorting names are now reversed due to their comments location
+		assertEquals("test2", name);
+		assertEquals("test1", name2);
 	}
 	
 	/**
@@ -57,4 +79,5 @@ public class SortingControllerTest extends TestCase {
 	public void testSortingPicture(){
 		
 	}
-}
+	}
+

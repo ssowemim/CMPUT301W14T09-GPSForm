64f5de72fdbdb969642fdcf085a592f49d567a17
ssowemim <ssowemim@ualberta.ca>
2014-04-07 18:04:33 -0600

    Fixed sorting controller by location and added sorting by picture

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/SortingControllerTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/SortingControllerTest.java
@@ -42,8 +42,8 @@ public class SortingControllerTest extends ActivityInstrumentationTestCase2<TopC
 		Bitmap bitmap =  BitmapFactory.decodeResource(this.getActivity().getResources(), R.drawable.no_img);
 		SerializableBitmap sBitmap = new SerializableBitmap(bitmap);
 		
-		geo.setLatitude(20.0);
-		geo.setLongitude(10.0);
+		geo.setLatitude(77.0);
+		geo.setLongitude(89.0);
 		lc1.setGeo(geo);
 		
 		comment = CommentFactory.buildComment(lc1, "test1", "testing1", true, sBitmap, true, "username");
@@ -71,13 +71,54 @@ public class SortingControllerTest extends ActivityInstrumentationTestCase2<TopC
 		// after sorting names are now reversed due to their comments location
 		assertEquals("test2", name);
 		assertEquals("test1", name2);
+		commList.clear();
+		testList.clear();
 	}
 	
 	/**
 	 * @author ssowemim
 	 */
 	public void testSortingPicture(){
+		Comment comment = null, comment1 = null;
+		LocationController lc = new LocationController();
+		LocationController lc1 = new LocationController();
+		LocationController lc2 = new LocationController();
 		
+		GeoLocation geo = new GeoLocation();
+		ArrayList<Comment> commList = new ArrayList<Comment>();
+		SortingController sc = new SortingController();
+		Bitmap bitmap =  BitmapFactory.decodeResource(this.getActivity().getResources(), R.drawable.no_img);
+		SerializableBitmap sBitmap = new SerializableBitmap(bitmap);
+		
+		geo.setLatitude(20.0);
+		geo.setLongitude(10.0);
+		lc1.setGeo(geo);
+		
+		comment = CommentFactory.buildComment(lc1, "test1", "testing1", true, sBitmap, false, "username");
+		commList.add(comment);
+
+		geo.setLatitude(25.0);
+		geo.setLongitude(12.0);
+		lc2.setGeo(geo);
+		
+		comment1 = CommentFactory.buildComment(lc2, "test2", "testing2", true, sBitmap, true, "username1");
+		commList.add(comment1);
+
+		geo.setLongitude(0);
+		geo.setLatitude(0);
+		lc.setGeo(geo);
+		
+		ArrayList<Comment> testList = sc.sortPicTopComments(commList);
+		String name = testList.get(0).getAuthorName();
+		String name2 = testList.get(1).getAuthorName();
+		
+		// names are put into the list as follows
+		assertTrue(commList.get(0).getAuthorName().equals("test2"));
+		assertTrue(commList.get(1).getAuthorName().equals("test1"));
+
+		// after sorting names are now reversed due to their comments location
+		assertEquals("test2", name);
+		assertEquals("test1", name2);
 	}
 	}
 

e502317d00ef962705054be33a320602150e7eba
ssowemim <ssowemim@ualberta.ca>
2014-04-07 18:37:32 -0600

    More tests in user profile model list

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/UserProfileModelListTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/UserProfileModelListTest.java
@@ -3,24 +3,20 @@ package ca.cmput301w14t09.test;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.test.ActivityInstrumentationTestCase2;
 import ca.cmput301w14t09.R;
 import ca.cmput301w14t09.TopCommentsActivity;
 import ca.cmput301w14t09.Model.UserProfileModel;
 import ca.cmput301w14t09.Model.UserProfileModelList;
-import junit.framework.TestCase;
 
 /**
- * 
+ * This junit test, tests the user profile model list.. checking to see if it initialized variables
+ * properly and pushes up to the server properly etc.
  * @author ssowemim
- *
  */
 public class UserProfileModelListTest extends ActivityInstrumentationTestCase2<TopCommentsActivity> {
-
 	public UserProfileModelListTest(){
-
 		super(TopCommentsActivity.class);
 
 	}
@@ -42,8 +38,15 @@ public class UserProfileModelListTest extends ActivityInstrumentationTestCase2<T
 								   BitmapFactory.decodeResource(this.getActivity().getResources(), R.drawable.no_img));
 		// Checking if the model was infact added to the list
 		list = testUPMList.getList();
-		//Checking to see that the list of the size and changed from zero
-		assertFalse(0 == list.size());
 		
+		//Checking to see that the list of the size and changed from zero
+		assertEquals(1, list.size());
+		
+		testUPMList.addUserProfile("UNIQUEID2", "Johnnie Barber2", "FeMale", "123456789912", 
+				   "bestba12rber@gmail.com", "My barber shop is the best in townok", 
+				   BitmapFactory.decodeResource(this.getActivity().getResources(), R.drawable.no_img));
+		
+		//Checking if it can handle multiple add user profile
+		assertEquals(2, list.size());
 	}
 }

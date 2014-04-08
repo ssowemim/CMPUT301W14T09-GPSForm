fd27e015fec9ae56b2db7f87f3f92372c7d70b82
ssowemim <ssowemim@ualberta.ca>
2014-04-07 18:52:48 -0600

    Fixed test load from file in fileloadingsavingtest junit class

--- CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/FileLoadingSavingTest.java
+++ CMPUT301W14T09TestTest/src/ca/cmput301w14t09/test/FileLoadingSavingTest.java
@@ -7,9 +7,8 @@ import ca.cmput301w14t09.Model.User;
 import android.test.ActivityInstrumentationTestCase2;
 
 /**
- * 
- * @author Chun-Han Lee
- * Testing the fileloading & filesaving methods 
+ *  Going to be testing if the file saving and also file loading method works in this junit test
+ * @author Chun-Han Lee & ssowemim
  */
 public class FileLoadingSavingTest extends ActivityInstrumentationTestCase2<TopCommentsActivity> {
 
@@ -18,16 +17,30 @@ public class FileLoadingSavingTest extends ActivityInstrumentationTestCase2<TopC
 	}
 	
 	/**
-	 * 
-	 * @author Chun-Han Lee
-	 * Testing Saving and Loading at the same time 
-	 * 
+	 * Testing to see if variables were initialized right.
+	 * Testing to see that the user was added successfully, this was done by getting the size
+	 * of the array of user name before you add (size) and get the size after you have added (newSize)
+	 * then simply compare (size+1) == new size.
+	 * @author Chun-Han Lee & ssowemim
 	 */
 	public void testLoadFromFile(){
-		User newuser = new User("hey");
-		FileSaving.appendUserNameToList("hey", getActivity());
-		String [] testList = FileLoading.loadFromFile(getActivity());
-		assertNotNull(testList);
+		User newUser = new User("hey");
+		
+		// Testing to see if variables were properly initialized.
+		String [] testList = null;
+		assertNull("This should return null", testList);
+		
+		//getting size of list before adding username
+		testList = FileLoading.loadFromFile(getActivity());
+		int size = testList.length;
+		
+		//Getting size of list after adding username
+		FileSaving.appendUserNameToList(newUser.getUserName(), getActivity());
+		testList = FileLoading.loadFromFile(getActivity());
+		int newSize = testList.length;
+		
+		//check to see if the old size+1 equals the new size
+		assertEquals(size+1, newSize);
 	}
 	
 	/**
